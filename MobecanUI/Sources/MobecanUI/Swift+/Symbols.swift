f3f64117a782600cb2c6a130cf81f7c1624b8d08
Dmitry Kotenko <d.kotenko@yandex.ru>
2020-06-27 01:56:56 +0300

    Useful symbols

---
+++ MobecanUI/Sources/MobecanUI/Swift+/Symbols.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2020 Mobecan. All rights reserved.
+
+public extension String {
+  
+  static var thinSpace: String { " " }
+  static var nonBreakingSpace: String { " " }
+  static var thinNonBreakingSpace: String { " " }
+  
+  static var minus: String { "−" }
+  static var rouble: String { "₽" }
+}
