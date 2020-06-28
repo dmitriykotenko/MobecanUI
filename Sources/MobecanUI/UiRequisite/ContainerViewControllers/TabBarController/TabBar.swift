2c45f52b9437a85b1e59dc876f070b825ba7ddbf
Dmitry Kotenko <d.kotenko@yandex.ru>
2020-06-28 17:22:56 +0300

    TabBar layout simplified

--- Sources/MobecanUI/UiRequisite/ContainerViewControllers/TabBarController/TabBar.swift
+++ Sources/MobecanUI/UiRequisite/ContainerViewControllers/TabBarController/TabBar.swift
@@ -14,48 +14,35 @@ public class TabBar: UIView {
 
   public var selectedTab: Driver<Tab?> { radioButton.selectedElement }
   public var tabTap: Signal<Tab> { radioButton.userDidSelectElement.filterNil() }
+
+  private let radioButton: RadioButton<Tab>
+  private let backgroundView: UIView
   
   public let contentHeight: CGFloat
 
-  private let backgroundView: UIView
-
-  private let radioButton: RadioButton<Tab>
-  
   public required init?(coder: NSCoder) { interfaceBuilderNotSupportedError() }
 
   public init(tabs: [Tab],
               backgroundView: UIView,
               initTabButton: @escaping (String?, UIImage?) -> TabButton,
-              contentHeight: CGFloat,
-              bottomInset: CGFloat) {
+              contentHeight: CGFloat) {
     self.backgroundView = backgroundView
     self.contentHeight = contentHeight
     
-    radioButton = RadioButton(
-      visibleElements: tabs,
-      createButton: { initTabButton($0.title, $0.icon) }
-    )
+    radioButton =
+      RadioButton(
+        visibleElements: tabs,
+        createButton: { initTabButton($0.title, $0.icon) }
+      )
+      .height(contentHeight)
 
     super.init(frame: .zero)
-   
-    _ = height(contentHeight + bottomInset)
     
     addSubviews()
   }
   
   private func addSubviews() {
-    addSubview(backgroundView)
-    
-    backgroundView.snp.makeConstraints {
-      $0.top.leading.trailing.equalToSuperview()
-      $0.bottom.greaterThanOrEqualToSuperview()
-    }
-    
-    addSubview(radioButton)
-    
-    radioButton.snp.makeConstraints {
-      $0.top.leading.trailing.equalToSuperview()
-      $0.height.equalTo(contentHeight)
-    }
+    putSubview(backgroundView)
+    putSubviewInsideSafeArea(radioButton)
   }
 }
