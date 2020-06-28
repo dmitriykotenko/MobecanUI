cb38f6e4c8afd93286508f0376ae6eaff7173e87
Dmitry Kotenko <d.kotenko@yandex.ru>
2020-06-28 17:13:32 +0300

    UIView.putSingleSubview() method

--- Sources/MobecanUI/Swift+/UIKit/UIView+singleSubview.swift
+++ Sources/MobecanUI/Swift+/UIKit/UIView+singleSubview.swift
@@ -1,47 +1,23 @@
 //  Copyright © 2020 Mobecan. All rights reserved.
 
-import RxSwift
-import SnapKit
 import UIKit
-
-
-public enum SubviewPosition {
-  
-  case atIndex(Int)
-  case above(UIView)
-  case below(UIView)
-}
 
 
 public extension UIView {
   
-  func putSubview(_ subview: UIView,
-                  _ position: SubviewPosition? = nil,
-                  insets: UIEdgeInsets = .zero) {
-    addSubview(subview, position: position)
+  func putSingleSubview(_ subview: UIView,
+                        _ position: SubviewPosition? = nil,
+                        insets: UIEdgeInsets = .zero) {
+    removeAllSubviews()
     
-    subview.snp.makeConstraints { $0.edges.equalToSuperview().inset(insets) }
+    putSubview(subview, position, insets: insets)
   }
   
-  func putSubviewInsideSafeArea(_ subview: UIView,
-                                _ position: SubviewPosition? = nil,
-                                insets: UIEdgeInsets = .zero) {
-    addSubview(subview, position: position)
+  func putSingleSubviewInsideSafeArea(_ subview: UIView,
+                                      _ position: SubviewPosition? = nil,
+                                      insets: UIEdgeInsets = .zero) {
+    removeAllSubviews()
     
-    subview.snp.makeConstraints { $0.edges.equalTo(safeAreaLayoutGuide).inset(insets) }
-  }
-  
-  private func addSubview(_ subview: UIView,
-                          position: SubviewPosition?) {
-    switch position {
-    case .atIndex(let index):
-      insertSubview(subview, at: index)
-    case .above(let anotherSubview):
-      insertSubview(subview, aboveSubview: anotherSubview)
-    case .below(let anotherSubview):
-      insertSubview(subview, belowSubview: anotherSubview)
-    case nil:
-      addSubview(subview)
-    }
+    putSubviewInsideSafeArea(subview, position, insets: insets)
   }
 }
