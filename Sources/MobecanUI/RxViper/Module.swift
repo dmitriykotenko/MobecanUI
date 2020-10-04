2253de39d21649c81e97894dad5eb49ac81ebf5a
Dmitry Kotenko <d.kotenko@yandex.ru>
2020-10-04 20:54:06 +0300

    Module.finishedAsSingle property

--- Sources/MobecanUI/RxViper/Module.swift
+++ Sources/MobecanUI/RxViper/Module.swift
@@ -1,5 +1,6 @@
 //  Copyright © 2020 Mobecan. All rights reserved.
 
+import RxCocoa
 import RxSwift
 import UIKit
 
@@ -8,6 +9,12 @@ public protocol Module {
   
   var viewController: UIViewController { get }
   var finished: Observable<Void> { get }
-  
+
   var demonstrator: Demonstrator? { get set }
+}
+
+
+extension Module {
+
+  var finishedAsSingle: Single<Void> { finished.take(1).asSingle() }
 }
