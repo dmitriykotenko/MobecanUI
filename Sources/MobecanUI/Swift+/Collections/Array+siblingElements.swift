7d8f67fc8cb3b5d445133f6b4e846c6bb5d9f405
Dmitry Kotenko <d.kotenko@yandex.ru>
2020-11-26 23:06:28 +0300

    Array.firstElement(after:) and Array.lastElement(before:)

---
+++ Sources/MobecanUI/Swift+/Collections/Array+siblingElements.swift
@@ -0,0 +1,13 @@
+//  Copyright © 2020 Mobecan. All rights reserved.
+
+
+public extension Array where Element: Equatable {
+
+  func firstElement(after element: Element) -> Element? {
+    drop { $0 != element }.drop { $0 == element }.first
+  }
+
+  func lastElement(before element: Element) -> Element? {
+    reversed().firstElement(after: element)
+  }
+}
