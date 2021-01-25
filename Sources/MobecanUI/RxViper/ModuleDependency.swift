347bb836bd2fbfe6b8c0b1e590fa180de87752ae
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-01-25 06:27:22 +0300

    More flexible ModuleDependency initializers

--- Sources/MobecanUI/RxViper/ModuleDependency.swift
+++ Sources/MobecanUI/RxViper/ModuleDependency.swift
@@ -12,6 +12,12 @@ public enum ModuleDependency {
     public var output: Observable<OutputValue>
     
     public init<SomeModule: Module>(_ module: SomeModule,
+                                    output: @escaping (SomeModule) -> Observable<OutputValue>) {
+      self.module = module
+      self.output = output(module)
+    }
+
+    public init<SomeModule: Module>(_ module: SomeModule,
                                     output: KeyPath<SomeModule, Observable<OutputValue>>) {
       self.module = module
       self.output = module[keyPath: output]
@@ -22,7 +28,13 @@ public enum ModuleDependency {
     
     public var module: Module
     public var output: Single<OutputValue>
-    
+
+    public init<SomeModule: Module>(_ module: SomeModule,
+                                    output: @escaping (SomeModule) -> Single<OutputValue>) {
+      self.module = module
+      self.output = output(module)
+    }
+
     public init<SomeModule: Module>(_ module: SomeModule,
                                     output: KeyPath<SomeModule, Single<OutputValue>>) {
       self.module = module
@@ -37,6 +49,14 @@ public enum ModuleDependency {
     public var output: Observable<OutputValue>
     
     public init<SomeModule: Module>(_ module: SomeModule,
+                                    input: @escaping (SomeModule) -> AnyObserver<InputValue>,
+                                    output: @escaping (SomeModule) -> Observable<OutputValue>) {
+      self.module = module
+      self.input = input(module)
+      self.output = output(module)
+    }
+
+    public init<SomeModule: Module>(_ module: SomeModule,
                                     input: KeyPath<SomeModule, AnyObserver<InputValue>>,
                                     output: KeyPath<SomeModule, Observable<OutputValue>>) {
       self.module = module
@@ -50,7 +70,15 @@ public enum ModuleDependency {
     public var module: MiniModule
     public var input: AnyObserver<InputValue>
     public var output: Observable<OutputValue>
-    
+
+    public init<SomeModule: MiniModule>(_ module: SomeModule,
+                                        input: @escaping (SomeModule) -> AnyObserver<InputValue>,
+                                        output: @escaping (SomeModule) -> Observable<OutputValue>) {
+      self.module = module
+      self.input = input(module)
+      self.output = output(module)
+    }
+
     public init<SomeModule: MiniModule>(_ module: SomeModule,
                                         input: KeyPath<SomeModule, AnyObserver<InputValue>>,
                                         output: KeyPath<SomeModule, Observable<OutputValue>>) {
@@ -63,15 +91,29 @@ public enum ModuleDependency {
 
 
 public extension Module {
-  
+
+  func and<OutputValue>(output: @escaping (Self) -> Observable<OutputValue>) -> ModuleDependency.Output<OutputValue> {
+    .init(self, output: output)
+  }
+
   func and<OutputValue>(output: KeyPath<Self, Observable<OutputValue>>) -> ModuleDependency.Output<OutputValue> {
     .init(self, output: output)
   }
-  
+
+  func and<OutputValue>(singleOutput: @escaping (Self) -> Single<OutputValue>)
+  -> ModuleDependency.OneTime<OutputValue> {
+    .init(self, output: singleOutput)
+  }
+
   func and<OutputValue>(singleOutput: KeyPath<Self, Single<OutputValue>>) -> ModuleDependency.OneTime<OutputValue> {
     .init(self, output: singleOutput)
   }
-  
+
+  func and<Input, Output>(input: @escaping (Self) -> AnyObserver<Input>,
+                          output: @escaping (Self) -> Observable<Output>) -> ModuleDependency.Pipe<Input, Output> {
+    .init(self, input: input, output: output)
+  }
+
   func and<Input, Output>(input: KeyPath<Self, AnyObserver<Input>>,
                           output: KeyPath<Self, Observable<Output>>) -> ModuleDependency.Pipe<Input, Output> {
     .init(self, input: input, output: output)
@@ -80,7 +122,12 @@ public extension Module {
 
 
 public extension MiniModule {
-  
+
+  func and<Input, Output>(input: @escaping (Self) -> AnyObserver<Input>,
+                          output: @escaping (Self) -> Observable<Output>) -> ModuleDependency.MiniPipe<Input, Output> {
+    .init(self, input: input, output: output)
+  }
+
   func and<Input, Output>(input: KeyPath<Self, AnyObserver<Input>>,
                           output: KeyPath<Self, Observable<Output>>) -> ModuleDependency.MiniPipe<Input, Output> {
     .init(self, input: input, output: output)
