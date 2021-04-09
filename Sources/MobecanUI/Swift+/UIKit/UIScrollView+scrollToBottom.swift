93c25f5436cb3d0de7ee01969bf4282be7e1088a
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-04-10 01:22:54 +0300

    UIScrollView.scrollToBottom()

---
+++ Sources/MobecanUI/Swift+/UIKit/UIScrollView+scrollToBottom.swift
@@ -0,0 +1,17 @@
+//  Copyright © 2020 Mobecan. All rights reserved.
+
+import UIKit
+
+
+public extension UIScrollView {
+
+  func scrollToBottom(animated: Bool) {
+    setContentOffset(
+      CGPoint(
+        x: 0,
+        y: contentSize.height - bounds.size.height + contentInset.bottom
+      ),
+      animated: animated
+    )
+  }
+}
