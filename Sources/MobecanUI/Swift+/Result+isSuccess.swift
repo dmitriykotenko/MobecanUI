c0e73092f84118d62bb41581ad019ebb8e70b083
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-04-09 21:19:49 +0300

    More diverse Result.isSuccess checks

--- Sources/MobecanUI/Swift+/Result+isSuccess.swift
+++ Sources/MobecanUI/Swift+/Result+isSuccess.swift
@@ -1,7 +1,7 @@
 //  Copyright © 2020 Mobecan. All rights reserved.
 
 
-extension Result {
+public extension Result {
 
   var isSuccess: Bool {
     switch self {
@@ -12,5 +12,29 @@ extension Result {
     }
   }
 
-  var isFailure: Bool { !isSuccess }
+  func isSuccessWhere(_ condition: (Success) -> Bool) -> Bool {
+    switch self {
+    case .success(let value) where condition(value):
+      return true
+    default:
+      return false
+    }
+  }
+
+  func isNotNilSuccess<Element>(and condition: (Element) -> Bool = { _ in true }) -> Bool where Success == Element? {
+    switch self {
+    case .success(let value?) where condition(value):
+      return true
+    default:
+      return false
+    }
+  }
+}
+
+
+public extension Result where Success: Equatable {
+
+  func isSuccess(_ value: Success) -> Bool {
+    isSuccessWhere { $0 == value }
+  }
 }
