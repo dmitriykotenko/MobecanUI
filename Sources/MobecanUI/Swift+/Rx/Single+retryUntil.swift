69373207f89a763d825ee42d8bde5b223a4661ad
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-04-09 21:18:55 +0300

    More convenient Single.retryUntilSuccess() method

--- Sources/MobecanUI/Swift+/Rx/Single+retryUntil.swift
+++ Sources/MobecanUI/Swift+/Rx/Single+retryUntil.swift
@@ -24,39 +24,33 @@ public extension Single {
 public extension Single {
   
   static func retryUntilSuccess<Value, SomeError: Error>(retryInterval: Duration,
-                                                         operation: @escaping () -> Single<Element>) -> Single<Element>
-    where Element == Result<Value, SomeError> {
-
-      retry(
-        until: {
-          switch $0 {
-          case .success:
-            return true
-          case .failure:
-            return false
-          }
-        },
-        retryInterval: retryInterval,
-        operation: operation
-      )
+                                                         operation: @escaping () -> Single<Element>) -> Single<Value>
+  where Element == Result<Value, SomeError> {
+    retry(
+      until: { $0.isSuccess },
+      retryInterval: retryInterval,
+      operation: operation
+    )
+    .asObservable()
+    .filterSuccess()
+    .asSingle()
   }
 
   static func retry<Value, SomeError: Error & Equatable>(untilSuccessOr error: SomeError,
                            retryInterval: Duration,
                            operation: @escaping () -> Single<Element>) -> Single<Element>
-    where Element == Result<Value, SomeError> {
-
-      retry(
-        until: {
-          switch $0 {
-          case .success, .failure(error):
-            return true
-          default:
-            return false
-          }
-        },
-        retryInterval: retryInterval,
-        operation: operation
-      )
+  where Element == Result<Value, SomeError> {
+    retry(
+      until: {
+        switch $0 {
+        case .success, .failure(error):
+          return true
+        default:
+          return false
+        }
+      },
+      retryInterval: retryInterval,
+      operation: operation
+    )
   }
 }
