0f7353b4b72714b1e1df6f29719c172b9ba2c94d
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-04-10 00:08:28 +0300

    zip() and flatten() for Optional

---
+++ Sources/MobecanUI/Swift+/Optional+flatten.swift
@@ -0,0 +1,9 @@
+//  Copyright © 2021 Mobecan. All rights reserved.
+
+
+public extension Optional {
+
+  func flatten<NestedValue>() -> NestedValue? where Wrapped == NestedValue? {
+    flatMap { $0 }
+  }
+}
