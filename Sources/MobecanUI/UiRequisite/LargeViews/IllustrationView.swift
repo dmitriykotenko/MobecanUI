42043f89cf412cbcafc1845fec35bc589767101f
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-04-18 21:20:36 +0300

    Customizable subviews alignment in IllustrationView

--- Sources/MobecanUI/UiRequisite/LargeViews/IllustrationView.swift
+++ Sources/MobecanUI/UiRequisite/LargeViews/IllustrationView.swift
@@ -6,21 +6,6 @@ import UIKit
 
 
 public class IllustrationView: UIView {
-  
-  public struct Subviews {
-    
-    public let illustrationView: UIImageView
-    public let titleLabel: UILabel
-    public let subtitleLabel: UILabel
-    
-    public init(illustrationView: UIImageView,
-                titleLabel: UILabel,
-                subtitleLabel: UILabel) {
-      self.illustrationView = illustrationView
-      self.titleLabel = titleLabel
-      self.subtitleLabel = subtitleLabel
-    }
-  }
 
   public var illustration: AnyObserver<UIImage?> { illustrationView.rx.image.asObserver() }
   public var title: AnyObserver<String?> { titleLabel.rx.text.asObserver() }
@@ -33,11 +18,10 @@ public class IllustrationView: UIView {
   public required init?(coder: NSCoder) { interfaceBuilderNotSupportedError() }
   
   public init(subviews: Subviews,
+              layout: Layout,
               illustration: UIImage? = nil,
               title: String? = nil,
-              subtitle: String? = nil,
-              illustrationToTitleSpacing: CGFloat = 0,
-              titleToSubtitleSpacing: CGFloat = 0) {
+              subtitle: String? = nil) {
     self.illustrationView = subviews.illustrationView
     self.titleLabel = subviews.titleLabel
     self.subtitleLabel = subviews.subtitleLabel
@@ -45,20 +29,56 @@ public class IllustrationView: UIView {
     super.init(frame: .zero)
     
     putSubview(
-      .vstack(
-        alignment: .center,
-        [
-          illustrationView,
-          .spacer(height: illustrationToTitleSpacing),
-          titleLabel,
-          .spacer(height: titleToSubtitleSpacing),
-          subtitleLabel
-        ]
-      )
+      layout.mainSubview(subviews: subviews)
     )
     
     illustration.map { self.illustration.onNext($0) }
     title.map { self.title.onNext($0) }
     subtitle.map { self.subtitle.onNext($0) }
+  }
+}
+
+
+public extension IllustrationView {
+
+  struct Subviews {
+
+    public let illustrationView: UIImageView
+    public let titleLabel: UILabel
+    public let subtitleLabel: UILabel
+
+    public init(illustrationView: UIImageView,
+                titleLabel: UILabel,
+                subtitleLabel: UILabel) {
+      self.illustrationView = illustrationView
+      self.titleLabel = titleLabel
+      self.subtitleLabel = subtitleLabel
+    }
+  }
+
+  struct Layout {
+
+    public var alignment: UIStackView.Alignment
+    public var illustrationToTitleSpacing: CGFloat
+    public var titleToSubtitleSpacing: CGFloat
+
+    static let standard = Layout(
+      alignment: .center,
+      illustrationToTitleSpacing: 0,
+      titleToSubtitleSpacing: 0
+    )
+
+    func mainSubview(subviews: Subviews) -> UIView {
+      .vstack(
+        alignment: alignment,
+        [
+          subviews.illustrationView,
+          .spacer(height: illustrationToTitleSpacing),
+          subviews.titleLabel,
+          .spacer(height: titleToSubtitleSpacing),
+          subviews.subtitleLabel
+        ]
+      )
+    }
   }
 }
