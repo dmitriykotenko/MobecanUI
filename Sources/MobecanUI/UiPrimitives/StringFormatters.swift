8b36e19bc34b4d3a495adbacdcc5733a9647f314
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-04-23 22:55:00 +0300

    Xcode 12 build issue fixed

--- Sources/MobecanUI/UiPrimitives/StringFormatters.swift
+++ Sources/MobecanUI/UiPrimitives/StringFormatters.swift
@@ -16,11 +16,13 @@ public class PhoneNumberFormatter: StringFormatter {
   public init() {}
 
   public func format(_ string: String) -> String {
-    string.prefix(2) + " "
-    + string.dropFirst(2).prefix(3) + " "
-    + string.dropFirst(5).prefix(3) + "-"
-    + string.dropFirst(8).prefix(2) + "-"
-    + string.dropFirst(10)
+    let s1 = string.prefix(2) + " "
+    let s2 = string.dropFirst(2).prefix(3) + " "
+    let s3 = string.dropFirst(5).prefix(3) + "-"
+    let s4 = string.dropFirst(8).prefix(2) + "-"
+    let s5 = string.dropFirst(10)
+
+    return String(s1 + s2 + s3 + s4 + s5)
   }
 }
 
