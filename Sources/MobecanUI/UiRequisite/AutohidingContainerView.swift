e43800c27d4cd0c6a5ba2a8231ba20faa1efc0f9
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-05-07 01:59:04 +0300

    More convenient syntax for AutohidingContainerView layout

--- Sources/MobecanUI/UiRequisite/AutohidingContainerView.swift
+++ Sources/MobecanUI/UiRequisite/AutohidingContainerView.swift
@@ -14,21 +14,15 @@ public class AutohidingContainerView: ClickThroughView {
   public required init?(coder: NSCoder) { interfaceBuilderNotSupportedError() }
 
   public init(_ subview: UIView,
-              layout: (UIView) -> (UIView, UIEdgeInsets)) {
+              layout: (UIView) -> UIView = { $0 },
+              insets: UIEdgeInsets = .zero) {
 
     super.init(frame: .zero)
 
-    let (mainSubview, insets) = layout(subview)
-
-    putSubview(mainSubview, insets: insets)
+    putSubview(layout(subview), insets: insets)
 
     visibilityListener = subview.observe(\.isHidden, options: .initial) { [weak self] view, _ in
       self?.isHidden = view.isHidden
     }
-  }
-
-  public convenience init(_ subview: UIView,
-                          insets: UIEdgeInsets = .zero) {
-    self.init(subview) { ($0, insets) }
   }
 }
