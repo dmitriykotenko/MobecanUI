a9b722801760283d0368a15c9ae54d84024a3e6f
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-10-15 00:27:04 +0300

    More concise initializer for UIColor

---
+++ Sources/MobecanUI/Swift+/UIKit/UIColor+conciseInit.swift
@@ -0,0 +1,16 @@
+//  Copyright © 2021 Mobecan. All rights reserved.
+
+import UIKit
+
+
+public extension UIColor {
+
+  convenience init(r: Int, g: Int, b: Int, alpha: CGFloat) {
+    self.init(
+      red: CGFloat(r) / 255.0,
+      green: CGFloat(g) / 255.0,
+      blue: CGFloat(b) / 255.0,
+      alpha: alpha
+    )
+  }
+}
