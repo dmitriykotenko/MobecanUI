b260c58498f8e4a333c3fd8b7802feb94c7d2795
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-10-28 15:30:15 +0300

    Price renamed to Money

--- Sources/MobecanUI/Entities/Money.swift
+++ Sources/MobecanUI/Entities/Money.swift
@@ -3,7 +3,7 @@
 import Foundation
 
 
-public struct Price: Equatable, Hashable, Codable, Lensable, CustomStringConvertible {
+public struct Money: Equatable, Hashable, Codable, Lensable, CustomStringConvertible {
 
   public var amount: Decimal
   public var currency: Currency
@@ -16,15 +16,15 @@ public struct Price: Equatable, Hashable, Codable, Lensable, CustomStringConvert
     self.currency = currency
   }
 
-  public static func roubles(_ amount: Decimal) -> Price {
-    Price(
+  public static func roubles(_ amount: Decimal) -> Money {
+    Money(
       amount: amount,
       currency: .rouble
     )
   }
   
-  public static func dollars(_ amount: Decimal) -> Price {
-    Price(
+  public static func dollars(_ amount: Decimal) -> Money {
+    Money(
       amount: amount,
       currency: .dollar
     )
@@ -38,4 +38,18 @@ public struct Price: Equatable, Hashable, Codable, Lensable, CustomStringConvert
       return [currency.symbol] + " \(amount)"
     }
   }
+}
+
+
+public extension Int {
+
+  var roubles: Money { .roubles(Decimal(self)) }
+  var dollars: Money { .dollars(Decimal(self)) }
+}
+
+
+public extension Decimal {
+
+  var roubles: Money { .roubles(self) }
+  var dollars: Money { .dollars(self) }
 }
