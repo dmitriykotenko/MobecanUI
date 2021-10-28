34aa3b35dc2a925caebf403a2fdb2632fa3371cb
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-10-28 15:11:45 +0300

    'Gender' entity

---
+++ Sources/MobecanUI/Entities/Gender.swift
@@ -0,0 +1,14 @@
+// Copyright © 2020 Mobecan. All rights reserved.
+
+import Foundation
+
+
+/// Пол человека.
+public enum Gender: String, Equatable, Hashable, Codable {
+
+  /// Мужчина.
+  case male
+
+  /// Женщина.
+  case female
+}
