3994c294ee810a3c6a11be79b37f7c7f0d396bc3
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-11-09 19:54:53 +0300

    Inversion of array of Result-s

--- Sources/MobecanUI/Swift+/Collections/Array+result.swift
+++ Sources/MobecanUI/Swift+/Collections/Array+result.swift
@@ -14,4 +14,25 @@ public extension Array {
       if case .success(let value) = $0 { return value } else { return nil }
     }
   }
+
+  func successesAndErrors<Value, SomeError: Error>() -> ([Value], [SomeError])
+  where Element == Result<Value, SomeError> {
+    (successes(), errors())
+  }
+
+  /// Превращает [Result<Value, SomeError>] в Result<[Value], SomeError>.
+  func invert<Value, SomeError: Error>() -> Result<[Value], SomeError> where Element == Result<Value, SomeError> {
+    let initialValue: Result<[Value], SomeError> = .success([])
+
+    return reduce(initialValue) { all, next in
+      switch (all, next) {
+      case (.success(let allValues), .success(let nextValue)):
+        return .success(allValues + [nextValue])
+      case (.failure(let error), _):
+        return .failure(error)
+      case (_, .failure(let error)):
+        return .failure(error)
+      }
+    }
+  }
 }
