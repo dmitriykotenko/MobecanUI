a6c86d51e2418455122c23dcc975c57c3d14a4cc
Dmitry Kotenko <d.kotenko@yandex.ru>
2021-11-17 21:43:46 +0300

    .hstack() and .vstack() migrated to LayoutKit

--- Sources/MobecanUI/UiPrimitives/Stacks/UIView+hstacks+vstacks.swift
+++ Sources/MobecanUI/UiPrimitives/Stacks/UIView+hstacks+vstacks.swift
@@ -1,5 +1,6 @@
 //  Copyright © 2020 Mobecan. All rights reserved.
 
+import LayoutKit
 import SnapKit
 import UIKit
 
@@ -11,16 +12,14 @@ public extension UIView {
                                          spacing: CGFloat? = nil,
                                          _ subviews: Subviews,
                                          insets: UIEdgeInsets = .zero) -> UIView where Subviews.Element == UIView {
-    .zstack([
-      stackView(
-        axis: .horizontal,
-        alignment: alignment,
-        distribution: distribution,
-        spacing: spacing,
-        subviews: subviews,
-        insets: insets
-      )
-    ])
+    fastStackView(
+      axis: .horizontal,
+      alignment: alignment,
+      distribution: distribution,
+      spacing: spacing,
+      subviews: subviews,
+      insets: insets
+    )
   }
   
   /// Returns horizontal stack
@@ -54,16 +53,14 @@ public extension UIView {
                                          spacing: CGFloat? = nil,
                                          _ subviews: Subviews,
                                          insets: UIEdgeInsets = .zero) -> UIView where Subviews.Element == UIView {
-    .zstack([
-      stackView(
-        axis: .vertical,
-        alignment: alignment,
-        distribution: distribution,
-        spacing: spacing,
-        subviews: subviews,
-        insets: insets
-      )
-    ])
+    fastStackView(
+      axis: .vertical,
+      alignment: alignment,
+      distribution: distribution,
+      spacing: spacing,
+      subviews: subviews,
+      insets: insets
+    )
   }
   
   private static func stackView<Subviews: Sequence>(axis: NSLayoutConstraint.Axis,
@@ -86,5 +83,94 @@ public extension UIView {
       stack.layoutMargins = insets
       
       return stack
+  }
+
+  private static func fastStackView<Subviews: Sequence>(axis: NSLayoutConstraint.Axis,
+                                                        alignment: UIStackView.Alignment = .fill,
+                                                        distribution: UIStackView.Distribution? = nil,
+                                                        spacing: CGFloat? = nil,
+                                                        subviews: Subviews,
+                                                        intrinsicWidth: CGFloat? = nil,
+                                                        intrinsicHeight: CGFloat? = nil,
+                                                        insets: UIEdgeInsets = .zero)
+  -> StackView where Subviews.Element == UIView {
+
+    let stack = StackView(
+      axis: axis.asLayoutKitAxis,
+      spacing: spacing ?? 0,
+      distribution: distribution?.asLayoutKitDistribution ?? .fillFlexing,
+      contentInsets: insets,
+      childrenAlignment: alignment.asLayoutKitAlignment,
+      flexibility: .inflexible,
+      intrinsicWidth: intrinsicWidth,
+      intrinsicHeight: intrinsicHeight
+    )
+
+    stack.addArrangedSubviews(Array(subviews))
+
+    return stack
+  }
+}
+
+
+private extension NSLayoutConstraint.Axis {
+
+  var asLayoutKitAxis: LayoutKit.Axis {
+    switch self {
+    case .horizontal:
+      return .horizontal
+    case .vertical:
+      return .vertical
+    @unknown default:
+      fatalError("NSLayoutConstraint.Axis \"\(self)\" is not supported")
+    }
+  }
+}
+
+
+private extension UIStackView.Alignment {
+
+  var asLayoutKitAlignment: LayoutKit.Alignment {
+    switch self {
+    case .leading:
+      return .fillLeading
+    case .trailing:
+      return .fillTrailing
+    case .top:
+      return .topFill
+    case .bottom:
+      return .bottomFill
+    case .center:
+      return .center
+    case .fill:
+      return .fill
+    case .firstBaseline:
+      return .topFill
+    case .lastBaseline:
+      return .bottomFill
+    @unknown default:
+      fatalError("UIStackView.Alignment \"\(self)\" is not supported")
+    }
+  }
+}
+
+
+private extension UIStackView.Distribution {
+
+  var asLayoutKitDistribution: LayoutKit.StackLayoutDistribution {
+    switch self {
+    case .fill:
+      return .fillFlexing
+    case .equalCentering:
+      return .fillEqualSize
+    case .equalSpacing:
+      return .fillEqualSpacing
+    case .fillEqually:
+      return .fillEqualSize
+    case .fillProportionally:
+      return .fillFlexing
+    @unknown default:
+      fatalError("UIStackView.Distribution \"\(self)\" is not supported")
+    }
   }
 }
