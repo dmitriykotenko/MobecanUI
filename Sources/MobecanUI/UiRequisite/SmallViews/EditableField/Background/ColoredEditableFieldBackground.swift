13770ea810df9a23d682f678fe00483df1a8a325
Dmitry Kotenko <d.kotenko@yandex.ru>
2022-02-09 00:08:10 +0300

    More customizable ColoredEditableFieldBackground

---
+++ Sources/MobecanUI/UiRequisite/SmallViews/EditableField/Background/ColoredEditableFieldBackground.swift
@@ -0,0 +1,34 @@
+// Copyright © 2020 Mobecan. All rights reserved.
+
+import LayoutKit
+import RxCocoa
+import RxSwift
+import UIKit
+
+
+open class ColoredEditableFieldBackground: LayoutableView, EditableFieldBackgroundProtocol {
+
+  open lazy var state: AnyObserver<State> = .onNext { [weak self, tintColors] in
+    self?.updateTintColor(tintColors[$0] ?? .clear)
+  }
+
+  private let tintColors: [State: UIColor]
+  
+  public required init?(coder: NSCoder) { interfaceBuilderNotSupportedError() }
+
+  public init(tintColors: [State: UIColor]) {
+    self.tintColors = tintColors
+
+    super.init()
+
+    self
+      .backgroundColor(.lightGray)
+      .cornerRadius(10)
+  }
+
+  open func updateTintColor(_ tintColor: UIColor) {
+    border(
+      .init(color: tintColor, width: 1)
+    )
+  }
+}
