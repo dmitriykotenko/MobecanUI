2a9e613c2ae45cad339441b42842a33c80de4a58
Dmitry Kotenko <d.kotenko@yandex.ru>
2022-03-22 17:36:06 +0300

    UITextView.imitateTextFieldInset()

--- Sources/MobecanUI/UiPrimitives/Builders/UITextView+restyling.swift
+++ Sources/MobecanUI/UiPrimitives/Builders/UITextView+restyling.swift
@@ -4,14 +4,21 @@ import UIKit
 
 
 public extension UITextView {
-  
-  // Fixes horizontal and vertical insets of text view, so it looks like UITextField.
-  // Thorough explanation: https://stackoverflow.com/questions/746670/how-to-lose-margin-padding-in-uitextview
+
+  /// Adjusts text insets, so the text view looks like UITextField with given height.
+  func imitateTextFieldInset(textFieldHeight: CGFloat) -> Self {
+    removeDefaultTextInset()
+      .adjustVerticalTextInset(minimumHeight: textFieldHeight)
+  }
+
+  /// Completely removes default text insets of text view, so it looks more like UITextField.
+  ///
+  /// Thorough explanation: https://stackoverflow.com/questions/746670/how-to-lose-margin-padding-in-uitextview
   func removeDefaultTextInset() -> Self {
     textContainerInset(.zero).lineFragmentPadding(0)
   }
 
-  /// Adjusts vertical text alignment, so the text view looks like UITextField with same minimum height.
+  /// Adjusts vertical text alignment.
   func adjustVerticalTextInset(minimumHeight: CGFloat) -> Self {
     guard let font = font else { return self }
     
