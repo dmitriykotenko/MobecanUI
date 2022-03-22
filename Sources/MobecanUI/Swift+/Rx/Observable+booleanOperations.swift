e2e44b35522baf63b3cb18de618a91c4f83a8f37
Dmitry Kotenko <d.kotenko@yandex.ru>
2022-03-22 16:10:15 +0300

    Boolean operations for SharedSequence and for Single

--- Sources/MobecanUI/Swift+/Rx/Observable+booleanOperations.swift
+++ Sources/MobecanUI/Swift+/Rx/Observable+booleanOperations.swift
@@ -35,3 +35,52 @@ public extension ObservableType where Element == Bool {
     observable.not()
   }
 }
+
+
+public extension SharedSequenceConvertibleType where Element == Bool {
+
+  func and<That: SharedSequenceConvertibleType>(_ that: That) -> SharedSequence<SharingStrategy, Bool>
+  where That.SharingStrategy == SharingStrategy, That.Element == Bool {
+
+    SharedSequence.combineLatest(self, that) { $0 && $1 }
+  }
+
+  func or<That: SharedSequenceConvertibleType>(_ that: That) -> SharedSequence<SharingStrategy, Bool>
+  where That.SharingStrategy == SharingStrategy, That.Element == Bool {
+
+    SharedSequence.combineLatest(self, that) { $0 || $1 }
+  }
+
+  func not() -> SharedSequence<SharingStrategy, Bool> {
+    map { !$0 }
+  }
+
+  static func not<SomeSequence: SharedSequenceConvertibleType>(_ sequence: SomeSequence)
+  -> SharedSequence<SharingStrategy, Bool>
+  where SomeSequence.SharingStrategy == SharingStrategy, SomeSequence.Element == Bool {
+
+    sequence.not()
+  }
+}
+
+
+public extension PrimitiveSequence where Trait == SingleTrait, Element == Bool {
+
+  func and(_ that: Single<Bool>) -> Single<Bool> {
+    self.asObservable().and(that.asObservable())
+      .asSingle()
+  }
+
+  func or(_ that: Single<Bool>) -> Single<Bool> {
+    self.asObservable().or(that.asObservable())
+      .asSingle()
+  }
+
+  func not() -> Single<Bool> {
+    map { !$0 }
+  }
+
+  static func not(_ sequence: Single<Bool>) -> Single<Bool> {
+    sequence.not()
+  }
+}
