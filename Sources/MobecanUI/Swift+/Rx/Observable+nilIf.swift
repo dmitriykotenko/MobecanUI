185ec7807fcc04bdd181cc8723e0958da75638a9
Dmitry Kotenko <d.kotenko@yandex.ru>
2022-05-15 23:45:02 +0300

    More generic reactive .nilIf() operator

--- Sources/MobecanUI/Swift+/Rx/Observable+nilIf.swift
+++ Sources/MobecanUI/Swift+/Rx/Observable+nilIf.swift
@@ -1,27 +1,72 @@
 // Copyright © 2020 Mobecan. All rights reserved.
 
 import RxSwift
+import RxCocoa
 
 
-public extension Observable {
+public extension ObservableType {
 
-  func nilIf<T>(_ predicate: Observable<Bool>) -> Observable<T?> where T? == Element {
-    Observable.combineLatest(predicate, self) { $0 ? nil : $1 }
+  func nilIf<T, Predicate: ObservableConvertibleType>(_ predicate: Predicate) -> Observable<T?>
+  where T? == Element, Predicate.Element == Bool {
+
+    Observable.combineLatest(predicate.asObservable(), self) { $0 ? nil : $1 }
   }
 
-  func nilIfNot<T>(_ predicate: Observable<Bool>) -> Observable<T?> where T? == Element {
-    nilIf(predicate.map { !$0 })
+  func nilIfNot<T, Predicate: ObservableConvertibleType>(_ predicate: Predicate) -> Observable<T?>
+  where T? == Element, Predicate.Element == Bool {
+
+    nilIf(predicate.asObservable().map { !$0 })
   }
 
-  func nilIf(_ predicate: Observable<Bool>) -> Observable<Element?> {
+  func nilIf<Predicate: ObservableConvertibleType>(_ predicate: Predicate) -> Observable<Element?>
+  where Predicate.Element == Bool {
+
     let optionalSelf: Observable<Element?> = map { $0 }
     
     return optionalSelf.nilIf(predicate)
   }
   
-  func nilIfNot(_ predicate: Observable<Bool>) -> Observable<Element?> {
+  func nilIfNot<Predicate: ObservableConvertibleType>(_ predicate: Predicate) -> Observable<Element?>
+  where Predicate.Element == Bool {
+
     let optionalSelf: Observable<Element?> = map { $0 }
     
+    return optionalSelf.nilIfNot(predicate)
+  }
+}
+
+
+public extension SharedSequenceConvertibleType {
+
+  func nilIf<T, Predicate: SharedSequenceConvertibleType>(_ predicate: Predicate)
+  -> SharedSequence<SharingStrategy, T?>
+  where T? == Element, Predicate.Element == Bool, Predicate.SharingStrategy == SharingStrategy {
+
+    SharedSequence.combineLatest(predicate, self) { $0 ? nil : $1 }
+  }
+
+  func nilIfNot<T, Predicate: SharedSequenceConvertibleType>(_ predicate: Predicate)
+  -> SharedSequence<SharingStrategy, T?>
+  where T? == Element, Predicate.Element == Bool, Predicate.SharingStrategy == SharingStrategy {
+
+    nilIf(predicate.map { !$0 })
+  }
+
+  func nilIf<Predicate: SharedSequenceConvertibleType>(_ predicate: Predicate)
+  -> SharedSequence<SharingStrategy, Element?>
+  where Predicate.Element == Bool, Predicate.SharingStrategy == SharingStrategy {
+
+    let optionalSelf: SharedSequence<SharingStrategy, Element?> = map { $0 }
+
+    return optionalSelf.nilIf(predicate)
+  }
+
+  func nilIfNot<Predicate: SharedSequenceConvertibleType>(_ predicate: Predicate)
+  -> SharedSequence<SharingStrategy, Element?>
+  where Predicate.Element == Bool, Predicate.SharingStrategy == SharingStrategy {
+
+    let optionalSelf: SharedSequence<SharingStrategy, Element?> = map { $0 }
+
     return optionalSelf.nilIfNot(predicate)
   }
 }
