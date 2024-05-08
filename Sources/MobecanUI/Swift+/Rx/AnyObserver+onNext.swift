e80da1aa00bec327283faaf3a7bb345f6f53dd66
Dmitry Kotenko <d.kotenko@yandex.ru>
2024-05-08 15:03:48 +0300

    Xcode 15's build warning fixed

--- Sources/MobecanUI/Swift+/Rx/AnyObserver+onNext.swift
+++ Sources/MobecanUI/Swift+/Rx/AnyObserver+onNext.swift
@@ -5,7 +5,7 @@ import RxSwift
 
 public extension AnyObserver {
   
-  static func onNext<Element>(handler: @escaping (Element) -> Void) -> AnyObserver<Element> {
+  static func onNext(handler: @escaping (Element) -> Void) -> AnyObserver<Element> {
     AnyObserver<Element> {
       if case .next(let element) = $0 {
         handler(element)
