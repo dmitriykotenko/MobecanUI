28ee8d17ebefe1fa078245f22633c3e29251e580
Dmitry Kotenko <d.kotenko@yandex.ru>
2025-05-07 03:32:20 +0300

    Xcode-16-compatible fork of Kingfisher 7.12

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     .package(url: "https://github.com/RxSwiftCommunity/RxOptional", from: .init(5, 0, 4)),
     .package(url: "https://github.com/RxSwiftCommunity/RxGesture", from: .init(4, 0, 2)),
     .package(url: "https://github.com/RxSwiftCommunity/RxKeyboard", from: .init(2, 0, 0)),
-    .package(url: "https://github.com/onevcat/Kingfisher", from: .init(7, 11, 0)),
+    .package(url: "https://github.com/dmitriykotenko/Kingfisher", branch: "7.12xcode16"),
     .package(url: "https://github.com/SnapKit/SnapKit", from: .init(5, 7, 1)),
     .package(url: "https://github.com/dmitriykotenko/LayoutKit", branch: "feature/swift-5.3"),
     .package(url: "https://github.com/dmitriykotenko/SwiftDateTime", from: .init(0, 2, 2))
