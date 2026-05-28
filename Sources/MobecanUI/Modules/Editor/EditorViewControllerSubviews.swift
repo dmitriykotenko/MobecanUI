f295111ce0bd5c8f5f13696a486c858f284f0b20
Dmitry Kotenko <d.kotenko@yandex.ru>
2026-05-28 14:09:31 +0300

    'topTrailingView' added to EditorViewControllerSubviews

--- Sources/MobecanUI/Modules/Editor/EditorViewControllerSubviews.swift
+++ Sources/MobecanUI/Modules/Editor/EditorViewControllerSubviews.swift
@@ -9,15 +9,18 @@ public struct EditorViewControllerSubviews {
   
   public var editorView: UIView
   public var closeButton: UIButton?
+  public var topTrailingView: UIView?
   public var finalizeButtonContainer: LoadingButtonContainer
   public var initScrollView: () -> UIScrollView
 
   public init(editorView: UIView,
               closeButton: UIButton?,
+              topTrailingView: UIView? = nil,
               finalizeButtonContainer: LoadingButtonContainer,
               initScrollView: @escaping () -> UIScrollView) {
     self.editorView = editorView
     self.closeButton = closeButton
+    self.topTrailingView = topTrailingView
     self.finalizeButtonContainer = finalizeButtonContainer
     self.initScrollView = initScrollView
   }
